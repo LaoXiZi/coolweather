4fda415db123a1c7970ea3fb54a953143b271712
Sun <754516746@qq.com>
2017-06-28 09:25:47 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/sq/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.sq.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ *  * Author：Elt on 2017/6/27 18:13
+ */
+
+public class Province extends DataSupport {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    private String provinceName;
+    private int provinceCode;
+
+}
